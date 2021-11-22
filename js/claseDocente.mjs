7b09abda3490007268edf55a6f3c4a79bf1d36fd
John Jairo Rodriguez Medina <johnjairorodriguez384@gmail.com>
2021-11-21 23:32:08 -0500

    Empieza manejo de modulos con Js

---
+++ js/claseDocente.mjs
@@ -0,0 +1,79 @@
+export {docente};
+export {maestrosPlatzi};
+
+class docente {
+    #nombre; // solo se hace efectivo cuando se crea el getter o el setter
+    #cursosDicta; //el arra curso dicta tendrá la misma advertencia
+    constructor({nombre,cursosDicta = [],})
+    {
+    this.#nombre = nombre;
+    this.#cursosDicta = cursosDicta;
+    }
+    get getname(){
+        return this.#nombre;
+    }
+    set setname(cambiaNombre){
+        if(typeof cambiaNombre !== "string"){
+            return console.log("El nuevo docente no es valido, porfavor verifica su nombre")
+        }
+        if(!cambiaNombre){
+            return console.log("El espacio del docente está vacio, porfavor verifica")
+        }
+        return this.#nombre = cambiaNombre;
+    }
+    get getCursoDicta(){
+        return this.#cursosDicta;
+    }
+    set setCursoDicta(cambiaCursoDicta){
+        for(let valores of cambiaCursoDicta){
+            if(typeof valores !== "string"){
+                return console.log("Los valores del array de clases no son cadenas de texto.")
+            }
+        }
+        if(cambiaCursoDicta.length === 0){
+            return console.log("No agregaste ningun curso a la modificacion")
+        }
+        if(!(cambiaCursoDicta instanceof Array)){
+            return console.log("No has iniciado un array")
+        }
+        return this.#cursosDicta = cambiaCursoDicta;
+    }
+}
+
+
+//import { docente } from "./claseDocente.mjs";
+
+const maestrosPlatzi = [
+    new docente({
+        nombre: "Freddy Vega",
+        cursosDicta : [
+        "Curso prog Basica",
+        "curso profesional de git y git hub",
+        "curso de introduccion a la web",
+    ]}),
+    new docente({
+        nombre: "Oscar Barajas",
+        cursosDicta : [
+            "Curso de ES6+",
+        ]
+    }),
+    new docente({
+        nombre: "Diego De granda",
+        cursosDicta: [
+        "Curso basico Js",
+        "Curso practico js",
+        "curso definitivo de Html"
+    ]}),
+    new docente({
+        nombre: "Andrew Pilsen",
+        cursosDicta: [
+        "curso de computacion Basica",
+        "Curso de algoritmos y pensamiento logico",
+        "Curso de tecnologia para gerentes",
+    ]})
+]
+
+console.log(maestrosPlatzi[0].getname)
+console.log(maestrosPlatzi[0].setname = "Andres Pelaez")// en este momento el nombre del docente de platzi de maestrosPlatzi[0] que era freddy vega cambio a andres pelaez
+console.log(maestrosPlatzi[0].getCursoDicta)
+console.log(maestrosPlatzi[0].setCursoDicta = ["curso unico de software", "curso de google analytics", "curso profesional de AI"])
--- js/main.mjs
+++ js/main.mjs
@@ -1,82 +1,10 @@
-// nueva
-// proxima clase a fabricar, la clase docente, con el nombre y los cursos que dicta
-class docente {
-    #nombre; // solo se hace efectivo cuando se crea el getter o el setter
-    #cursosDicta; //el arra curso dicta tendrá la misma advertencia
-    constructor({nombre,cursosDicta = [],})
-    {
-    this.#nombre = nombre;
-    this.#cursosDicta = cursosDicta;
-    }
-    get getname(){
-        return this.#nombre;
-    }
-    set setname(cambiaNombre){
-        if(typeof cambiaNombre !== "string"){
-            return console.log("El nuevo docente no es valido, porfavor verifica su nombre")
-        }
-        if(!cambiaNombre){
-            return console.log("El espacio del docente está vacio, porfavor verifica")
-        }
-        return this.#nombre = cambiaNombre;
-    }
-    get getCursoDicta(){
-        return this.#cursosDicta;
-    }
-    set setCursoDicta(cambiaCursoDicta){
-        for(let valores of cambiaCursoDicta){
-            if(typeof valores !== "string"){
-                return console.log("Los valores del array de clases no son cadenas de texto.")
-            }
-        }
-        if(cambiaCursoDicta.length === 0){
-            return console.log("No agregaste ningun curso a la modificacion")
-        }
-        if(!(cambiaCursoDicta instanceof Array)){
-            return console.log("No has iniciado un array")
-        }
-        return this.#cursosDicta = cambiaCursoDicta;
-    }
-}
+// Inicio del documento Js // Manejo en modulos permite generar un mayor orden de la informacion
+// por el momento el import y el export funciona a traves de nombre los archivos con la extension .mjs
 
-const maestrosPlatzi = [
-    new docente({
-        nombre: "Freddy Vega",
-        cursosDicta : [
-        "Curso prog Basica",
-        "curso profesional de git y git hub",
-        "curso de introduccion a la web",
-    ]}),
-    new docente({
-        nombre: "Oscar Barajas",
-        cursosDicta : [
-            "Curso de ES6+",
-        ]
-    }),
-    new docente({
-        nombre: "Diego De granda",
-        cursosDicta: [
-        "Curso basico Js",
-        "Curso practico js",
-        "curso definitivo de Html"
-    ]}),
-    new docente({
-        nombre: "Andrew Pilsen",
-        cursosDicta: [
-        "curso de computacion Basica",
-        "Curso de algoritmos y pensamiento logico",
-        "Curso de tecnologia para gerentes",
-    ]})
-]
-
-console.log(maestrosPlatzi[0].getname)
-console.log(maestrosPlatzi[0].setname = "Andres Pelaez")// en este momento el nombre del docente de platzi de maestrosPlatzi[0] que era freddy vega cambio a andres pelaez
-console.log(maestrosPlatzi[0].getCursoDicta)
-console.log(maestrosPlatzi[0].setCursoDicta = ["curso unico de software", "curso de google analytics", "curso profesional de AI"])
-//console.log(maestrosPlatzi[0])
-//import { carro } from "./variables.mjs";
-//console.log(carro)
-/* cursos */
+// La clase docente que se encuentra en el modulo(archivo) claseDocente.mjs desarrolla un prototipo para instancias de objetos de los maestrosPlatzi
+import { docente } from "./claseDocente.mjs";
+// en el array maestrosPlatzi se crean las instancias de la clase derivadas de el prototipo (clase) docente que se encuentra en el mismo archivo(modulo)
+import { maestrosPlatzi } from "./claseDocente.mjs";
 
 
 
